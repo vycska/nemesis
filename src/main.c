dddc5bd69abe653f3c8b658383d39e141624d5e9
vytas <vycska@gmail.com>
2018-10-23 22:29:31 +0300

    Dar vienas pataisymas

--- src/main.c
+++ src/main.c
@@ -73,7 +73,7 @@ void main(void) {
       UART_Transmit("firmware file not present", 1);
    }
 
-   VTOR = (0x1000<<7);
+   VTOR = (0x1000);
    _dsb();
    _set_msp(*((unsigned int*)0x1000));
    _dsb();
