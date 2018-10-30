32c710fca749d4daec4468b4bcbe376dd262cff9
vytas <vycska@gmail.com>
2018-10-30 19:40:49 +0200

    Dar biski pataisiau plot.r

--- plot.r
+++ plot.r
@@ -1,6 +1,6 @@
 rm(list=ls())
 
-file <- readline("enter file: ")
+file <- readline("Failas: ")
 
 size <- file.size(file)
 blocks <- size/16;
@@ -9,7 +9,7 @@ f <- readBin(file, "raw", size, 1)
 
 df <- data.frame(date=integer(blocks), ds18b20_temp=integer(blocks), bme280_temp=integer(blocks), ds3231_temp=integer(blocks), bme280_hum=integer(blocks), bme280_pres=integer(blocks), adc_bat=integer(blocks))
 
-for(i in 0:(size/16-1)) {
+for(i in 0:(blocks-1)) {
    df$date[i+1] <- readBin(f[(i*16+1) : (i*16+4)], "integer", 1, 4, endian="little")
    df$ds18b20_temp[i+1] <- readBin(f[(i*16+5) : (i*16+6)], "integer", 1, 2, endian="little")
    df$bme280_temp[i+1] <- readBin(f[(i*16+7) : (i*16+8)], "integer", 1, 2, endian="little")
@@ -30,42 +30,49 @@ df$adc_bat <- df$adc_bat / 4095 * 3.3 * 2
 repeat {
 
 print(unique(as.Date(df$date)))
-
 print(table(as.Date(df$date)))
 
-day <- as.POSIXct(readline("enter day: "), origin="1970-01-01", tz="GMT")
-type <- readline("enter plot [1 - temp, 2 - hum, 3 - pres, 4 - bat]: ")
+day <- as.Date(readline("Dienos data: "), tz="GMT")
+type <- readline("Grafikas [1 - temp, 2 - hum, 3 - pres, 4 - bat]: ")
 
-if(!(as.Date(day,tz="GMT")%in%as.Date(df$date,tz="GMT")) || !(type==1 || type==2 || type==3 || type==4)) break;
+if(!(day %in% as.Date(df$date,tz="GMT")) || !(type==1 || type==2 || type==3 || type==4)) break;
 
-filter <- (as.Date(df$date,tz="GMT")==as.Date(day,tz="GMT"))
+filter <- (as.Date(df$date,tz="GMT") == day)
 x <- df$date[filter]
 l <- length(x)
 
 if(type==1) {
-   baly <- "temperatura, C"
+   baly <- expression("Temperatūra, " * degree * "C")
    mn <- min(df$ds18b20_temp[filter],df$bme280_temp[filter],df$ds3231_temp[filter])
    mx <- max(df$ds18b20_temp[filter],df$bme280_temp[filter],df$ds3231_temp[filter])
    mily <- c(mn-5, mx+5)
+   print(summary(df$ds18b20_temp[filter]))
+   print(summary(df$bme280_temp[filter]))
+   print(summary(df$ds3231_temp[filter]))
 } else if(type==2) {
-   baly <- "santykine dregmen, %"
+   baly <- "Santykinė drėgmė, %"
    mn <- min(df$bme280_hum[filter])
    mx <- max(df$bme280_hum[filter])
    mily <- c(mn-10, mx+10)
+   print(summary(df$bme280_hum[filter]))
 } else if(type==3) {
-   baly <- "slegis, mmHg"
+   baly <- "Slėgis, mmHg"
    mn <- min(df$bme280_pres[filter])
    mx <- max(df$bme280_pres[filter])
    mily <- c(mn-10, mx+10)
+   print(summary(df$bme280_pres[filter]))
 } else if(type==4) {
-   baly <- "baterija, V"
+   baly <- "Li-ion baterijos įtampa, V"
    mn <- min(df$adc_bat[filter])
    mx <- max(df$adc_bat[filter])
    mily <- c(mn-1,mx+1)
+   print(summary(df$adc_bat[filter]))
 }
 
-plot(0, 0, type="n", main=day, xlab="laikas", ylab=baly, xlim=c(day, day+86400), ylim=mily, xaxt="n")
+day <- as.POSIXct(day, tz="GMT")
+plot(0, 0, type="n", main=format(day, "%F [%A]"), xlab="Laikas", ylab=baly, xlim=c(day, day+86400), ylim=mily, xaxt="n")
 axis(side=1, at=seq(day, day+86400, len=9), labels=format(seq(day, day+86400, len=9),"%H:%M",tz="GMT"))
+abline(h=seq(as.integer(par('usr')[3])+1, as.integer(par('usr')[4])+1, by=1), lty=3)
 k <- 1
 for(i in 2:(l+1)) {
    if(i==l+1 || x[i] != x[i-1]+60) {
@@ -78,7 +85,7 @@ for(i in 2:(l+1)) {
       } else if(type==3) {
          lines((df$date[filter])[k:(i-1)], (df$bme280_pres[filter])[k:(i-1)], lwd=2, col="red")
       } else if(type==4) {
-         lines((df$date[filter])[k:(i-1)], (df$adc_bat[filter])[k:(i-1)], lwd=2, col="black")
+         lines((df$date[filter])[k:(i-1)], (df$adc_bat[filter])[k:(i-1)], lwd=2, col="red")
       }
       abline(v=x[k], lty=2)
       abline(v=x[i-1], lty=2)
@@ -94,7 +101,7 @@ if(type==1) {
          m <- (which(df[filter,t] == ifelse(i==1,mn,mx)))
          if(length(m)>0) {
             points((df$date[filter])[m[1]], (df[filter,t])[m[1]], pch=19, col="black")
-            text((df$date[filter])[m[1]], (df[filter,t])[m[1]], sprintf("%.1f [%s]", (df[filter,t])[m[1]], format((df$date[filter])[m[1]], "%H:%M", tz="GMT")), pos=ifelse(i==1,1,3))
+            text((df$date[filter])[m[1]], (df[filter,t])[m[1]], sprintf("%.2f [%s]", (df[filter,t])[m[1]], format((df$date[filter])[m[1]], "%H:%M", tz="GMT")), pos=ifelse(i==1,1,3))
          }
       }
    }
